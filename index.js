ee2e61f99ea36e420b94107eab5e4491442e4342
Grace Brackman <gbrackma@purdue.edu>
2018-05-17 11:36:13 -0400

    add flicks to the beg of list rather than the end

--- index.js
+++ index.js
@@ -26,11 +26,12 @@ const app = {
         name: f.flickName.value,
       }
   
-      this.flicks.push(flick)
+      this.flicks.unshift(flick)
       const item = this.renderListItem(flick)
-      this.list.appendChild(item)
+      this.list.insertBefore(item, this.list.firstElementChild)
   
       f.reset()
+      f.flickName.focus()
     },
   }
   
