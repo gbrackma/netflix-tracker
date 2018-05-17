89aa006a340613dea5d077141a2801b2c0322bd5
Grace Brackman <gbrackma@purdue.edu>
2018-05-17 12:03:16 -0400

    clone the template when adding the flick

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = {
       this.max = 0
       this.flicks = []
       this.list = document.querySelector(selectors.listSelector)
+      this.template = document.querySelector(selectors.templateSelector)
   
       document
         .querySelector(selectors.formSelector)
@@ -13,8 +14,8 @@ const app = {
     },
   
     renderListItem(flick) {
-      const item = document.createElement('li')
-      item.textContent = flick.name
+      const item = this.template.cloneNode(true)
+      item.querySelector('.flickName').textContent = flick.name
       item.dataset.id = flick.id
       return item
     },
@@ -38,4 +39,5 @@ const app = {
   app.init({
     formSelector: '#flickForm',
     listSelector: '#flickList',
+    templateSelector: '.flick.template'
   })
