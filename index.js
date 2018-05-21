4a91d4ddf0d16a073cd04d9eacc4bd0a90f51847
Grace Brackman <gbrackma@purdue.edu>
2018-05-20 22:42:09 -0400

    add work up and down buttons

--- index.js
+++ index.js
@@ -26,15 +26,16 @@ const app = {
       item
         .querySelector('.up')
         .addEventListener('click', ev => {
+            console.log('up')
             ev.preventDefault()
-            this.handleUp(rec, ev)
+            this.handleUp(item, ev)
         })
 
         item
         .querySelector('.down')
         .addEventListener('click', ev => {
             ev.preventDefault()
-            this.handleDown(rec, ev)
+            this.handleDown(item, ev)
         })
       
       item
@@ -89,21 +90,36 @@ const app = {
     handleFav(movie, ev){
 
         if( movie.fav == true){
-            ev.target.parentElement.parentElement.style.color = 'black'
+            ev.target.parentElement.parentElement.style.backgroundColor = "white"
+            ev.target.parentElement.parentElement.style.color = "rgb(185, 10, 10)"
             movie.fav = false
             return
         }
 
         movie.fav = true
-        ev.target.parentElement.parentElement.style.color = 'red';
+        ev.target.parentElement.parentElement.style.backgroundColor = "rgb(185, 10, 10)"
+        ev.target.parentElement.parentElement.style.color = "white"
     },
 
     handleUp(movie, ev){
 
+        //debugger
+        const oneBefore = movie.previousSibling
+
+        movie.remove
+        this.list.insertBefore(movie, oneBefore)
+        //ev.target.parentElement.parentElement.remove()
         
     },
 
     handleDown(movie, ev){
+
+        const oneAfter = movie.nextSibling.nextSibling
+
+        movie.remove
+        this.list.insertBefore(movie, oneAfter)
+
+        //nextSibling
 
         
     },
